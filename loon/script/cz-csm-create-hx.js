a9682e518e525c8d4fabd1f59f80c197cd69bc7d
bufx <bufanxiang@gmail.com>
2022-12-06 09:34:59 +0800

    Create cz-csm-create-hx.js

---
+++ loon/script/cz-csm-create-hx.js
@@ -0,0 +1,4 @@
+let body = $response.body;
+body='{\"msg\":\"success\",\"data\":{\"statusCode\":1,\"checkTime\":\"2022-12-06 02:01:06.0\",\"isShow48HoursHs\":0,\"isShow72HoursHs\":0},\"code\":0}';
+
+$done({body});
