ff08917b45ef7d72788fc256e8105681756d6488
bufx <bufanxiang@gmail.com>
2022-12-09 08:01:46 +0800

    更新 cz-csm-create-hx.js

--- loon/script/cz-csm-create-hx.js
+++ loon/script/cz-csm-create-hx.js
@@ -1,4 +1,4 @@
 let body = $response.body;
-body='{\"msg\":\"success\",\"data\":{\"statusCode\":1,\"checkTime\":\"2022-12-06 02:01:06.0\",\"isShow48HoursHs\":0,\"isShow72HoursHs\":0},\"code\":0}';
+body='{\"msg\":\"success\",\"data\":{\"statusCode\":1,\"checkTime\":\"2022-12-09 02:01:06.0\",\"isShow48HoursHs\":0,\"isShow72HoursHs\":0},\"code\":0}';
 
 $done({body});
